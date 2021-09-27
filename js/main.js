5facbbb2ab40c7f7043f857e55b49f71491dfc3f
MacUser <macuser@MacUsers-MBP.attlocal.net>
2021-09-27 18:54:48 -0500

    Spaceman Random Words Generating

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 /*----- constants -----*/
-const words = [];
+const words = ['CAT', 'DOG', 'HUMAN', 'BUG', 'BEAR'];
 
 /*----- app's state (variables) -----*/
 let board;
 let winner;
   
 /*----- cached element references -----*/
-const letterEls = document.querySelectorAll('#ltr' > section).textContent;
+const letterEls = document.querySelectorAll('#ltr > button');
 const msgEl = document.querySelector('h2');
-const btnEl = document.querySelector('#replay' > section);
-  
+const replayEl = document.getElementById('#replay');
+const astroPic = document.getElementById('#astro');
+
 /*----- event listeners -----*/
-document.getElementById('#ltr')
+document.querySelector('#ltr')
   .addEventListener('click', letterClick);
   
 /*----- functions -----*/
@@ -20,21 +21,51 @@ init();
 function init() {
     
 board = [
-    [null, null, null],                     // cat
-    [null, null, null],                     // dog
-    [null, null, null, null, null],         // human
-    [null, null, null],                     // bug
-    [null, null, null, null],               // bear
+    ['_', '_', '_'],                     // cat
+    ['_', '_', '_'],                     // dog
+    ['_', '_', '_', '_', '_'],         // human
+    ['_', '_', '_'],                     // bug
+    ['_', '_', '_', '_'],               // bear
 ];
 winner = null;
 render();
 }
 
 function letterClick(evt)   {
-    const letters = letterEls.indexOf(evt.target);
+    const buttonText = evt.target.innerText;
+    console.log(buttonText);
+
+    const wordIndex = randomWordIndex();
 
 }
 
 function render() {
-    btnEl.style.visibility = winner ? 'visible' : 'hidden';
+    // replayEl.style.visibility = winner ? 'visible' : 'hidden';
+    console.log(board[findWordsIndex]);
+    renderWord(findWordsIndex);
   }
+
+function renderWord(wordIndex) {
+
+  let wordMsg = board[wordIndex].toString();
+  for (let i = 0; i < wordMsg.length; i++) {
+      wordMsg = wordMsg.replace(',', ' ');
+  }
+  msgEl.innerHTML = wordMsg;
+}
+
+function randomWordIndex() {
+  const chosenWord = words[Math.floor(Math.random() * words.length)];
+  // console.log(chosenWord);
+  const findWordsIndex = words.findIndex(word => word === chosenWord);
+  // console.log(findWordsIndex);
+  return findWordsIndex;
+}
+
+function boardChange (buttonText, wordIndex) {
+  const chosenWord = word[wordIndex];
+  if (chosenWord.includes(buttonText)) {
+    // board[wordIndex][0] = ;
+  }
+  board[wordIndex];
+}
