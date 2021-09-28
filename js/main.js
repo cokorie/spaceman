d5e497aa27338c544553a1ab5b274f2157a0c5e9
MacUser <macuser@MacUsers-MBP.attlocal.net>
2021-09-28 12:52:08 -0500

    Updated Letter Click and Win Logic

--- js/main.js
+++ js/main.js
@@ -1,29 +1,15 @@
 /*----- constants -----*/
-const words = ['THE BIRD IS THE WORD', 'TOP OF THE MORNING', 'I AM ONLY HUMAN', 'HAVE A GREAT DAY',
-  'LOVE IS THE ANSWER', 'THE MONSTER MASH', 'THE SOLAR SYSTEM', 'THE MILKY WAY', 'ANDROMEDA',
-  'EYE OF SAURON', 'ROCKETMAN', 'TRIANGULUM GALAXY', 'SCORPIUS', 'MERCURY', 'APOLLO THIRTEEN',
-  'MAE JEMISON', 'YVONNE CAGLE', 'NEIL ARMSTRONG', 'STRAIGHT TO THE MOON', 'IF YOU ARE READING THIS GREAT JOB',
-  'SHOOT FOR THE STARS', 'AIM FOR THE MOON', 'EXTRA TERRESTRIAL', 'WE ARE NOT ALONE', 'STAY UP AND STAY POSITIVE',
-  'YOU ARE DOING GREAT', 'GUARDIANS OF THE GALAXY', 'AVENGERS ASSEMBLE'];
-const MAX_WRONG_GUESSES = 6;
-const SPRITE_WIDTH = 504;
+const words = ['CAT', 'DOG', 'HUMAN', 'BUG', 'BEAR'];
 
 /*----- app's state (variables) -----*/
-let secretWord;
-let guessWord;
-let winOrLoss;
-let wrongLetters;
-
+let board;
+let winner;
+  
 /*----- cached element references -----*/
 const letterEls = document.querySelectorAll('#ltr > button');
 const msgEl = document.querySelector('h2');
-const replayEl = document.getElementById('replay');
-let astroPic = document.getElementById('astro');
-const guessEl = document.getElementById('guess');
-const laserPlayer = new Audio('http://www.freesound.org/data/previews/42/42106_70164-lq.mp3');
-const sirenPlayer = new Audio('http://www.freesound.org/data/previews/336/336899_4939433-lq.mp3');
-const clickPlayer = new Audio('https://www.fesliyanstudios.com/play-mp3/387');
-// const wrongPlayer = new Audio('https://www.sfxbuzz.com/jdownloads/Buzzer,%20Alarm%20and%20Siren%20Sound%20Effects/Wrong%20Clakson%20Sound%20Effect.mp3');
+const replayEl = document.getElementById('#replay');
+const astroPic = document.getElementById('#astro');
 
 /*----- event listeners -----*/
 document.querySelector('#ltr').addEventListener('click', letterClick);
@@ -34,15 +20,16 @@ replayEl.addEventListener('click', init);
 init();
 
 function init() {
-  secretWord = words[Math.floor(Math.random() * words.length)];
-  winOrLoss = null;
-  wrongLetters = '';
-  guessWord = '';
-  for (let letter of secretWord) {
-    guessWord += letter === ' ' ? ' ' : '_';
-
-  }
-  render();
+    
+board = [
+    ['_', '_', '_'],                     // cat
+    ['_', '_', '_'],                     // dog
+    ['_', '_', '_', '_', '_'],         // human
+    ['_', '_', '_'],                     // bug
+    ['_', '_', '_', '_'],               // bear
+];
+winner = null;
+render();
 }
 
 function letterClick(evt) {
@@ -75,42 +62,32 @@ function letterClick(evt) {
 }
 
 function render() {
-  replayEl.style.visibility = winOrLoss ? 'visible' : 'hidden';
-  guessEl.textContent = guessWord;
-  letterEls.forEach((btn) => {
-    if (guessWord.includes(btn.innerText)) {
-      btn.style.backgroundColor = 'green';
-      clickPlayer.play();
-    } else if (wrongLetters.includes(btn.innerText)) {
-      btn.style.backgroundColor = 'red';
-      // wrongPlayer.play();
-      clickPlayer.play();
-    } else {
-      btn.style.backgroundColor = 'gray';
-    }
-  });
-  astroPic.style.backgroundPositionX = `${-SPRITE_WIDTH * wrongLetters.length}px`;
-  renderMsg();
+    // replayEl.style.visibility = winner ? 'visible' : 'hidden';
+    console.log(board[findWordsIndex]);
+    renderWord(findWordsIndex);
+  }
+
+function renderWord(wordIndex) {
+
+  let wordMsg = board[wordIndex].toString();
+  for (let i = 0; i < wordMsg.length; i++) {
+      wordMsg = wordMsg.replace(',', ' ');
+  }
+  msgEl.innerHTML = wordMsg;
 }
 
-function getWinOrLoss() {
-  if (secretWord === guessWord) {
-    return 'W';
-  } else if (wrongLetters.length === MAX_WRONG_GUESSES) {
-    return 'L';
-  } else {
-    return null;
-  }
+function randomWordIndex() {
+  const chosenWord = words[Math.floor(Math.random() * words.length)];
+  // console.log(chosenWord);
+  const findWordsIndex = words.findIndex(word => word === chosenWord);
+  // console.log(findWordsIndex);
+  return findWordsIndex;
 }
 
-function renderMsg() {
-  if (winOrLoss === 'W') {
-    msgEl.innerHTML = `CONGRATULATIONS! YOU'VE WON!`;
-    laserPlayer.play();
-  } else if (winOrLoss === 'L') {
-    msgEl.innerHTML = 'GAME OVER! TRY AGAIN!';
-    sirenPlayer.play();
-  } else {
-    msgEl.innerHTML = `${6 - wrongLetters.length} guesses left!`;
+function boardChange (buttonText, wordIndex) {
+  const chosenWord = word[wordIndex];
+  if (chosenWord.includes(buttonText)) {
+    // board[wordIndex][0] = ;
   }
+  board[wordIndex];
 }
