0844e3f59f693a2f922819667d104bc9858ca5e3
MacUser <macuser@MacUsers-MBP.attlocal.net>
2021-09-28 12:52:08 -0500

    Updated Letter Click and Win Logic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 /*----- constants -----*/
-const words = ['CAT', 'DOG', 'HUMAN', 'BUG', 'BEAR'];
+const words = ['CAT', 'DOG', 'HUMAN', 'BUG', 'BEAR', 'THE MONSTER MASH'];
+const MAX_WRONG_NUMBERS = 6;
 
 /*----- app's state (variables) -----*/
-let board;
-let winner;
+let secretWord;
+let guessWord;
+let winOrLoss;
+let wrongLetters;
   
 /*----- cached element references -----*/
 const letterEls = document.querySelectorAll('#ltr > button');
 const msgEl = document.querySelector('h2');
-const replayEl = document.getElementById('#replay');
-const astroPic = document.getElementById('#astro');
+const replayEl = document.getElementById('replay');
+const astroPic = document.getElementById('astro');
 
 /*----- event listeners -----*/
 document.querySelector('#ltr')
@@ -19,16 +22,14 @@ document.querySelector('#ltr')
 init();
 
 function init() {
-    
-board = [
-    ['_', '_', '_'],                     // cat
-    ['_', '_', '_'],                     // dog
-    ['_', '_', '_', '_', '_'],         // human
-    ['_', '_', '_'],                     // bug
-    ['_', '_', '_', '_'],               // bear
-];
-winner = null;
-render();
+    secretWord = words[Math.floor(Math.random() * words.length)];
+    winOrLoss = null;
+    wrongLetters = '';
+    guessWord = '';
+    for (let letter of secretWord) {
+      guessWord += letter === ' ' ? ' ' : '_';
+    } 
+    // render();
 }
 
 function letterClick(evt)   {
@@ -41,13 +42,13 @@ function letterClick(evt)   {
 
 function render() {
     // replayEl.style.visibility = winner ? 'visible' : 'hidden';
-    console.log(board[findWordsIndex]);
+    console.log(secretWord[findWordsIndex]);
     renderWord(findWordsIndex);
   }
 
 function renderWord(wordIndex) {
 
-  let wordMsg = board[wordIndex].toString();
+  let wordMsg = secretWord[wordIndex].toString();
   for (let i = 0; i < wordMsg.length; i++) {
       wordMsg = wordMsg.replace(',', ' ');
   }
@@ -55,17 +56,19 @@ function renderWord(wordIndex) {
 }
 
 function randomWordIndex() {
-  const chosenWord = words[Math.floor(Math.random() * words.length)];
-  // console.log(chosenWord);
   const findWordsIndex = words.findIndex(word => word === chosenWord);
   // console.log(findWordsIndex);
   return findWordsIndex;
 }
 
-function boardChange (buttonText, wordIndex) {
+function sWordChange (buttonText, wordIndex) {
   const chosenWord = word[wordIndex];
   if (chosenWord.includes(buttonText)) {
-    // board[wordIndex][0] = ;
+    // secretWord[wordIndex][0] = ;
   }
-  board[wordIndex];
+  secretWord[wordIndex];
+}
+
+function getWinOrLoss () {
+
 }
